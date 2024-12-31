1c0147bd25792b6a4c017a4f55cf529e87ab117c
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2024-12-31 18:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ // Write your code here\
+	let n = s.length;
+
+    // Iterate over each character in the string
+    for (let i = 0; i < n; ++i) {
+        let found = false;
+
+        // Check if the character repeats in the
+        // rest of the string
+        for (let j = 0; j < n; ++j) {
+            if (i !== j && s[i] === s[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        // If character does not repeat, return it
+        if (found === false) {
+            return s[i];
+        }
+    }
+
+    // If no such character is found, return '$'
+    return '$';
 }
-const input = prompt("Enter a string");
+const input = prompt("racecar");
 alert(firstNonRepeatedChar(input)); 
